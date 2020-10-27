88884c26ea9ec467d3a9b2081dd0dcbd362f89d7
nmtu3 <nmtu3@10.0.0.93>
2020-10-27 11:57:39 -0700

    First commit

---
+++ src/Class1.java
@@ -0,0 +1,11 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is first Git project");
+		System.out.println("for Java projects");
+	}
+	
+
+}
